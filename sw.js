fbeac6d9dcc85cdd7e57a206529732e08dc10bf9
yaroslavdidyk12-oss <yaroslavdidyk12@gmail.com>
2026-01-29 08:59:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = "plan-safe-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE = "plan-safe-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
